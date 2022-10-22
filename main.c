c16f17182477de43ce29f45c4d8918ee4a4d2b6b
Michprac <michal.poczta135@gmail.com>
2022-10-22 10:53:22 +0200

    Choosing operator +, -, *

--- main.c
+++ main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
+    char c;
     printf("Calculator. What do you want to do?\n");
+    printf("Please enter operator from this list: \n\"+\", \"-\", \"*\"\n");
+    printf("Your operator: ");
+    scanf("%c", &c);
+    if (c == '+')
+    {
+        printf("Addition selected.\n");
+    }
+    else if (c == '-')
+    {
+        printf("Subtraction selected.\n");
+    }
+    else if (c == '*')
+    {
+        printf("Multiplication selected.\n");
+    }
+    else
+    {
+        printf("There is no such operator :(\n");
+        exit(0);
+    }
+
     return 0;
 }
