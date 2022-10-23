426c6f66b037d41a223ebcd5b3d54f52916dc8d1
Michprac <michal.poczta135@gmail.com>
2022-10-23 18:37:55 +0200

    Added division, result variable, showing of the result

--- main.c
+++ main.c
@@ -1,30 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+
+void delay(int seconds)
+{
+    clock_t time_start = clock();
+    while(clock()< time_start+seconds*1000){}
+}
+
 
 int main()
 {
-    char c;
-    printf("Calculator. What do you want to do?\n");
-    printf("Please enter operator from this list: \n\"+\", \"-\", \"*\"\n");
+    char c_operator;
+    float a, b, result;
+    printf("Calculator.\n");
+    //Entering numbers //
+    printf("Please, enter the first number: ");
+    scanf("%f", &a);
+    printf("\nPlease, enter the second number: ");
+    scanf("%f", &b);
+    printf("\n");
+    // Choosing operator //
+    printf("Please enter operator from this list: \n\"+\", \"-\", \"*\", \"/\"\n");
     printf("Your operator: ");
-    scanf("%c", &c);
-    if (c == '+')
+    scanf("%s", &c_operator);
+    if (c_operator == '+')
     {
         printf("Addition selected.\n");
+        result = a + b;
     }
-    else if (c == '-')
+    else if (c_operator == '-')
     {
         printf("Subtraction selected.\n");
+        result = a - b;
     }
-    else if (c == '*')
+    else if (c_operator == '*')
     {
         printf("Multiplication selected.\n");
+        result = a * b;
+    }
+    else if (c_operator == '/')
+    {
+        printf("Division selected.\n");
+        result = a / b;
     }
     else
+
     {
         printf("There is no such operator :(\n");
         exit(0);
     }
 
+    printf("Process started. I'm thinking");
+    delay(1);
+    printf(".");
+    delay(1);
+    printf(".");
+    delay(1);
+    printf(".\n");
+    printf("Your result is %.2f\n", result);
     return 0;
 }
